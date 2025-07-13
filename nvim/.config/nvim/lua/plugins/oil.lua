474d73ad5b0cd0609b45180d0308e90482b07ca4
Matt Billings <mbillings@gmail.com>
2025-07-13 12:48:29 -0700

    add oil plugin

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,12 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  lazy = false,
+  config = function()
+    require('oil').setup()
+    vim.keymap.set("n", "<leader>-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
+  end,
+}
